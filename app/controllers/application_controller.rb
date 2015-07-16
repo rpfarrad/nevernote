1d57fce940be427a6d6bc76b40b3c783958ec37a
Dave Strus <dave@davestrus.com>
2015-07-16 16:11:49 -0400

    Add method to force authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authorize_user
+    if current_user.nil?
+      redirect_to login_path, :'alert-danger' => t('session.flash.unauthenticated')
+    end
+  end
 end
